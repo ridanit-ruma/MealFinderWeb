6f891cdc3628b41dbc3af85699626edc156c2fbe
ridanit-ruma <minenaturecrew@gmail.com>
2025-03-16 20:57:20 +0900

    .env.example에 SSL 설정 추가 및 .gitignore에 cert 디렉토리 추가, 서비스 워커의 리비전 업데이트

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hb4fbon149g"
+    "revision": "0.0ev2lojcpo8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
